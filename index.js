2406532c5f3611e66e0ba8468db8324ba83de604
sagun basnet <basnetsagun8@gmail.com>
2025-05-10 14:55:11 +0545

    added greet

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log("First");
 console.log("Second");
 console.log("third");
 console.log("forth--ongoing");
+
+// my current change
+function greet(name = "there") {
+  console.log(`hello ${name}`);
+}
